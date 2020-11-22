5cc04b77de32d4810cbf225ec58118b3a4a10c11
sikami <widyasari.listya@gmail.com>
2020-11-22 10:32:26 +0000

    deleted some comments and made Api Class

---
+++ src/main/TelegramGoodReads/Api/Api.java
@@ -0,0 +1,13 @@
+package Api;
+
+public class Api {
+    private String apiKey;
+
+    public Api() {
+        this.apiKey = "key=hgTduibWyDqLAgu1v6wnRQ";
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+}
--- src/main/TelegramGoodReads/Goodreads.java
+++ src/main/TelegramGoodReads/Goodreads.java
@@ -1,3 +1,4 @@
+import Api.Api;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
@@ -16,9 +17,10 @@ public class Goodreads {
     private Result result;
 
     public Goodreads(String bookname) {
+        Api api = new Api();
         this.bookname = bookname.trim().replace(" ", "+");
         this.author = "";
-        this.apiKey = "key=hgTduibWyDqLAgu1v6wnRQ" ;
+        this.apiKey = api.getApiKey() ;
         this.url = "https://www.goodreads.com/book/title.xml?";
         this.result = new Result();
     }
@@ -92,44 +94,3 @@ public class Goodreads {
     }
 
 }
-
-
-/**
- Get the reviews for a book given a title string
- Get an xml response that contains embed code for the iframe reviews widget, which shows an excerpt (first 300 characters) of the most popular reviews of a book for a given title/author. The book shown will be the most popular book that matches all the keywords in the input string. The reviews are from all known editions of the book.
- URL: https://www.goodreads.com/book/title.FORMAT    (sample url)
- HTTP method: GET
- Parameters:
- format: xml or json
- key: Developer key (required).
- title: The title of the book to lookup.
- author: The author name of the book to lookup. This is optional, but is recommended for accuracy.
- rating: Show only reviews with a particular rating (optional)
-
- API key and sample : "https://www.goodreads.com/book/isbn/\" + isbn + \"?key=hgTduibWyDqLAgu1v6wnRQ"
-
- sample without author: https://www.goodreads.com/book/title.xml?key=hgTduibWyDqLAgu1v6wnRQ&title=Hound+of+the+Baskervilles
- sample with author: https://www.goodreads.com/book/title.xml?author=Arthur+Conan+Doyle&key=hgTduibWyDqLAgu1v6wnRQ&title=Hound+of+the+Baskervilles
- **/
-
-
-//need to catch this
-//book title, average rating, description
-/**<book>
-<id>8921</id>
-<title>
-<![CDATA[
-The Hound of the Baskervilles (Sherlock Holmes, #5)
-]]>
-</title>
-
-
- <average_rating>4.12</average_rating>
-
-
- <description>
- <![CDATA[
- blah blah
- ]]>
- </description>
- **/
